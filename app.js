dae3074e777eca36e6757a1ec062224076f33297
muyuu <anticyborg@gmail.com>
2016-02-08 14:20:18 +0900

    add force option

--- app.js
+++ app.js
@@ -11,5 +11,14 @@
         interval: 3000,
         length: 3
     });
+
+    uiCarousel({
+        root: ".js-carousel03",
+        duration: 600,
+        interval: 3000,
+        length: 3,
+        force: true,
+        useDots: false
+    });
   });
 })(window, jQuery);
